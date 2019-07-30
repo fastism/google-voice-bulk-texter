798f6bb961802545f64d42c11dab0bb34597e0e5
fastism <40200202+fastism@users.noreply.github.com>
2019-07-31 01:44:59 +0700

    Update main.js

--- src/contentScripts/main.js
+++ src/contentScripts/main.js
@@ -6,13 +6,13 @@ const selectors = {
 	// google voice (in cases where there are two selectors, it is to support newer versions to older versions, left to right)
 	gvMessagesTab: 'a[aria-label^="Message"][role="tab"], div[aria-label^="Message"][role="tab"]',
 	gvNumInputButton: 'div[gv-id="send-new-message"]',
-	gvNumInput: 'gv-recipient-picker input[ng-show="ctrl.allowToAddRecipients()"], gv-recipient-picker input[placeholder="Type a name or phone number"]',
+	gvNumInput: 'gv-recipient-picker input[ng-show="ctrl.allowToAddRecipients()"]',
 	gvStartChatButton: 'gv-contact-list div[ng-class="::ctrl.CSS.SEND_TO_PHONE_NUMBER"]',
 	gvRecipientButton: 'div[aria-label="Select recipients"] .mat-chip-list-wrapper gmat-input-chip[gv-id="chip-phone-number"], div[aria-label="Select recipients"] md-chips md-chip button',
 	gvMessageEditor: 'textarea[aria-label="Type a message"]',
 	gvSendButtonOld: 'gv-icon-button[icon-name="send"] button[aria-label="Send message"]',
 	gvSendButtonNew: 'gv-icon-button-ng2[icon-name="send"][label="Send message"] button',
-	gvSendingNote: 'gv-message-item div[ng-if="ctrl.shouldDisplayTransmissionStatus()"] div[ng-if="!ctrl.isFailed()"]',
+	gvSendingNote: 'gv-message-item div[ng-if="ctrl.shouldDisplayTransmissionStatus()"] div[ng-if="!ctrl.isFailed()"]', //???
 	gvMostRecentMessages: 'div[gv-id="content"] div[gv-test-id="bubble"] gv-annotation',
 	gvChatLoadedHeader: 'gv-message-list-header p[gv-test-id="conversation-title"]',
 
